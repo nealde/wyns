58cdafcd5afa2242a52ef70a11b75926dd4eb303
Sarah Alamdari <salamda1@uw.edu>
2018-06-05 17:28:31 -0700

    flake8 compat

--- wyns/tests/test_wyns.py
+++ wyns/tests/test_wyns.py
@@ -23,40 +23,42 @@ class testKerasModels(unittest.TestCase):
     def test_benchmark(self):
         core.Benchmark.run(self.test_baseline_model)
 
+
 def test_load_data():
     data_file_name = "tweet_global_warming.doc"
-    try: 
+    try:
         core.load_data(data_file_name)
-    except (AssertionError):
-        print ("Upload a csv file") 
+    except(AssertionError):
+        print("Upload a csv file")
     else:
-        raise Exception ("Code only handles csv or hdf5 files")
+        raise Exception("Code only handles csv or hdf5 files")
+
 
 def test_data_setup():
     top_words = "100"
     max_words = "100"
-    try: 
+    try:
         core.data_setup(top_words, max_words)
-    except (AssertionError):
-        print ("Inputs must be type int") 
+    except(AssertionError):
+        print("Inputs must be type int")
+
 
 def test_read_data():
     data_file = [1, 2, 3]
-    try: 
+    try:
         core.read_data(data_file)
-    except (AssertionError):
-        print ("Input must be a pandas DataFrame")
+    except(AssertionError):
+        print("Input must be a pandas DataFrame")
+
 
 def test_build_dataset():
-    vocab = pd.DataFrame({'col1':['hey','there','human']})
-    try: 
-        core.build_dataset(vocab,1)
-    except (AssertionError):
-        print ("Inputs must be list. Use read_data if using a pd.DataFrame)")
-    else: 
-        raise Exception ("Make sure n_words is type int")
-    
-
+    vocab = pd.DataFrame({'col1': ['hey', 'there', 'human']})
+    try:
+        core.build_dataset(vocab, 1)
+    except(AssertionError):
+        print("Inputs must be list. Use read_data if using a pd.DataFrame)")
+    else:
+        raise Exception("Make sure n_words is type int")
 
 
 if __name__ == '__main__':
