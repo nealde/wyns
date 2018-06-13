7c59da862564e73c08c3b0b6fbb56330efeb125a
wesleybeckner <wesleybeckner@gmail.com>
2018-06-12 17:34:07 -0700

    wes update

---
+++ scripts/wesley/garbage/.api_token.py
@@ -0,0 +1,4 @@
+consumer_key = "NLiOe9HzH1B5dxuqMAnVgxW33"
+consumer_secret = "FnHTVArzwldHJkVQ6mfI061wUgMZcSrNqakf1MwC2GNuL1BA1Y"
+access_token_key = "3018876422-xtMvACDrk0eTkORt02VvvODML4dR2xh5qGx8apN"
+access_token_secret = "1x6Zr5oHpBHgPxe3p6B99CdXjg5iYBmYV9X8mEFLsWF2t"
