43d7a43502ef01091024bafb2953c9e067bc0322
Neal <nealde@uw.edu>
2018-06-19 14:16:27 -0700

    updated the get_tweets py file and added some model notebooks

---
+++ wyns/data/format_twits.py
@@ -0,0 +1,5 @@
+import pandas as pd
+data = pd.read_csv('tweets.txt', delimiter='~~n~~',engine='python', names=['text','long','lat'])
+print(data.head())
+#with open('tweets.txt') as f:
+#    for line in f.readline
